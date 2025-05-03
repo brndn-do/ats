810e9160a04f26687bdbc49fac9a0589d3a5ccd4
brndn-do <dobrandon05@gmail.com>
2025-05-02 19:15:20 -0500

    Implement POST /api/jobs

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const pool = require('./db')
-
+const pool = require("./db");
 const app = express();
-
 app.use(express.json());
 
 // simple query function with retries
@@ -13,12 +11,12 @@ async function queryWithRetry(query, params = [], retries = 3, delay = 1000) {
       const result = await pool.query(query, params);
       console.log("Query succeeded!");
       return result;
-    } catch(err) {
+    } catch (err) {
       if (attempt === retries - 1) {
         throw err;
       }
       console.log(`Retrying... Attempt ${attempt + 1} of ${retries}`);
-      await new Promise(resolve => setTimeout(resolve, delay));
+      await new Promise((resolve) => setTimeout(resolve, delay));
     }
   }
 }
@@ -27,16 +25,48 @@ app.get("/", (req, res) => {
   res.send("ATS Backend");
 });
 
+// /api/jobs GET
 app.get("/api/jobs", async (req, res) => {
-  console.log('GET request api/jobs')
+  console.log("GET request /api/jobs");
   try {
     const query = "SELECT * FROM jobs;";
-    const response = await queryWithRetry(query);
-    const jobs = response.rows;
-    res.json(jobs);
+    const result = await queryWithRetry(query);
+    const jobs = result.rows;
+    return res.json(jobs);
   } catch (err) {
     console.error("Error fetching jobs:", err);
-    res.status(500).json({ message: "Error fetching jobs" });
+    return res.status(500).json({ message: "Error fetching jobs" });
+  }
+});
+
+// /api/jobs POST
+app.post("/api/jobs", async (req, res) => {
+  console.log("POST request /api/jobs");
+  try {
+    body = req.body;
+    if (!("title" in body && "description" in body && "admin_id" in body))
+      return res.status(422).json({ error: "Missing required fields" });
+    if (
+      typeof body.title != "string" ||
+      typeof body.description != "string" ||
+      typeof body.admin_id != "number"
+    ) {
+      return res.status(422).json({ error: "Incorrect data type(s) in body" });
+    }
+    const query =
+      "INSERT INTO jobs (title, description, admin_id) VALUES ($1, $2, $3) RETURNING id";
+    const values = [body.title, body.description, body.admin_id];
+    const result = await queryWithRetry(query, values);
+    if (result.rowCount === 1) {
+      console.log("Insert successful");
+      return res
+        .status(201)
+        .json({ message: "Job created!", job_id: result.rows[0].id });
+    }
+    return res.status(500).json({ message: "Failed to insert job" });
+  } catch (err) {
+    console.error("Error posting job:", err);
+    return res.status(500).json({ message: "Error posting job" });
   }
 });
 
