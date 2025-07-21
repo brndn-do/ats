59af02e5b89c48ebb92ab5c209175bc959e8eafe
brndn-do <dobrandon05@gmail.com>
2025-07-21 11:07:56 -0500

    test(db): Implement unit tests for queryWithRetry

---
+++ backend/__tests__/unit/db.test.js
@@ -0,0 +1,57 @@
+import queryWithRetry from "../../db.js";
+
+import { Pool } from "pg";
+
+// Mocks the `pg` module. `new Pool()` will always return the same singleton
+// mock object, allowing tests to configure its behavior for database calls.
+jest.mock("pg", () => {
+  const mPool = { query: jest.fn() };
+  return { Pool: jest.fn(() => mPool) };
+});
+
+describe("queryWithRetry", () => {
+  const id = 1;
+  const query = `
+    SELECT * FROM jobs
+    WHERE id = $1;
+  `;
+  const params = [id];
+  const pool = new Pool();
+  const mockResolvedValue = {
+    rows: [],
+    rowCount: 1,
+  };
+  beforeEach(() => {
+    pool.query.mockClear();
+  });
+
+  it("should succeed on the first attempt", async () => {
+    pool.query.mockResolvedValueOnce(mockResolvedValue);
+    const dbResult = await queryWithRetry(query, params);
+    expect(dbResult).toBe(mockResolvedValue);
+    expect(pool.query).toHaveBeenCalledTimes(1);
+  });
+
+  it("should succeed after one retry if the first attempt fails", async () => {
+    pool.query.mockRejectedValueOnce(new Error("DB connection error"));
+    pool.query.mockResolvedValueOnce(mockResolvedValue);
+    const dbResult = await queryWithRetry(query, params);
+    expect(dbResult).toBe(mockResolvedValue);
+    expect(pool.query).toHaveBeenCalledTimes(2);
+  });
+
+  it("should throw an error after all retry attempts fail", async () => {
+    pool.query.mockRejectedValue(new Error("DB connection error"));
+    await expect(queryWithRetry(query, params)).rejects.toThrow(
+      "DB connection error"
+    );
+    expect(pool.query).toHaveBeenCalledTimes(3);
+  });
+
+  it("should correctly pass query and parameters to the database client", async () => {
+    pool.query.mockResolvedValueOnce(mockResolvedValue);
+    await queryWithRetry(query, params);
+    expect(pool.query).toHaveBeenCalledTimes(1);
+    expect(pool.query).toHaveBeenCalledWith(query, params);
+  });
+});
