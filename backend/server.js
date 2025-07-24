6485e286fccdf50136d44b6206ea302f5587b218
brndn-do <dobrandon05@gmail.com>
2025-07-24 10:04:41 -0500

    feat(server.js): add graceful shutdown (close pool)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,21 @@
 import app from "./app.js";
+import { pool } from "./db.js";
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+const gracefulShutdown = () => {
+  console.log("Received kill signal, shutting down gracefully.");
+  server.close(() => {
+    console.log("Closed out remaining connections.");
+    pool.end(() => {
+      console.log("Database pool has been closed.");
+      process.exit(0);
+    });
+  });
+};
+
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
